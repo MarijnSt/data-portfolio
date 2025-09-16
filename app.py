dbc5d0dd292dfe82b9ed1a14eafa68eb064c7f67
MarijnSt <marijn.stammeleer@gmail.com>
2025-09-16 14:22:24 +0200

    feat: init streamlit app

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("Portfolio Marijn Stammeleer")
+st.subheader("Aspirant Data Scientist")
+
+st.write("")
+st.write("Welcome to my portfolio!")
+
+st.write("At the end of last year I started to make the switch from web development to data science. Since then I have followed a bunch of different courses and I've built a few projects. This Streamlit app is a collection of these projects and will function as my portfolio.")
+st.write("")
+st.write("Feel free to take a look around and reach out to me if you have any questions or want to collaborate.")
+st.write("You can find me on [LinkedIn](https://www.linkedin.com/in/marijn-stammeleer) and on [GitHub](https://github.com/MarijnSt).")
+st.write("")
+st.write("Hope to hear from you soon!")
+st.write("Marijn")
